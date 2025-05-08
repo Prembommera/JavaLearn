a29aa2c6f3e67865521d99a7c0a09bf02661d554
Prem <premnathbommera@gmail.com>
2025-05-08 18:23:24 +0530

    Java if else conditional statement

---
+++ JavaIf.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class JavaIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		
+		if (a>b ) {
+			System.out.println("A is greater than b");
+			
+		}
+		else {
+			System.out.println(" b is greater than a");
+		}
+	}
+	
+
+}
