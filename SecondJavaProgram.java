0a14809fe9f5fbf4238f4d3e6ebc7e8a212fec19
Prem <premnathbommera@gmail.com>
2025-05-08 18:07:03 +0530

    Addition of Two Numbers

---
+++ SecondJavaProgram.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SecondJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	// add of two number using scanner class
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println("Addition of two numbers: " + (a+b));
+	}
+
+}
